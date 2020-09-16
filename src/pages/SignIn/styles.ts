8b683d11821eab4735e8b00c7c89fc63583b9edc
Benício Barreto <benicio.daniel@gmail.com>
2020-09-16 14:45:17 -0300

    Created Input and Button component. Added vector icons

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px; /** Para nada encostar nas laterais */
 `;
 
 export const Title = styled.Text`
