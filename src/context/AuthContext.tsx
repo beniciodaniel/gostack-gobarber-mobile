f35e69a546c61a3196055e474fd826c9a3ff8b7b
Benício Barreto <benicio.daniel@gmail.com>
2020-09-20 11:57:29 -0300

    Implementing privates routes

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -21,6 +21,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -30,6 +31,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -42,6 +44,8 @@ const AuthProvider: React.FC = ({ children }) => {
         setData({ token: token[1], user: JSON.parse(user[1]) });
         // return { token, user: JSON.parse(user) }; // user = string, logo precisa transformar em objeto;
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -70,7 +74,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
