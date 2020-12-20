8f02bc2a79f8dfa4300b887b4957e7b8fda015cb
Benício Barreto <benicio.daniel@gmail.com>
2020-12-20 19:57:03 -0300

    Fixed build gradle

--- android/app/src/main/java/com/appgobarber/MainActivity.java
+++ android/app/src/main/java/com/appgobarber/MainActivity.java
@@ -1,4 +1,4 @@
-package com.appgobarber;
+package com.appgobarberbybenidev;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
@@ -17,6 +17,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "appgobarber";
+    return "appgobarberbybenidev";
   }
 }
--- android/app/src/main/java/com/appgobarber/SplashActivity.java
+++ android/app/src/main/java/com/appgobarber/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.appgobarber;
+package com.appgobarberbybenidev;
 
 import android.content.Intent;
 import android.os.Bundle;
